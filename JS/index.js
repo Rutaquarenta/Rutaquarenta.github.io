41584a07db1650a11a4ce8966076d77a420d0c90
Rutaquarenta <126628969+Rutaquarenta@users.noreply.github.com>
2023-02-28 21:11:40 -0300

    initial commit

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+function mostrar(){
+    document.getElementById('videop1').style.display = 'block';
+
+}
+function ocultar(){
+    document.getElementById('videop1').style.display = 'none';
+
+}
+
+function mostrarf(){
+    document.getElementById('formulario1').style.display = 'block';
+
+}
+function ocultarf(){
+    document.getElementById('formulario1').style.display = 'none';
+
+}
